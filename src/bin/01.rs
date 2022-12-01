affc3ba2270b106a8d42f0a9e9e2db225ab60b8c
David Harks <dave@dwink.net>
2022-12-01 10:41:33 -0600

    Day 1 complete!

---
+++ src/bin/01.rs
@@ -0,0 +1,60 @@
+
+#[derive(Debug)]
+pub struct Elf {
+    snacks: Vec<u32>,
+}
+
+impl Elf {
+
+    pub fn calories(&self) -> u32 {
+        self.snacks.iter().sum()
+    }
+}
+
+pub fn parse_input(input: &str) -> Vec<Elf> {
+    input
+        .split("\n\n")
+        .map(|e| Elf { snacks: e.lines().filter_map(|l| l.parse::<u32>().ok()).collect() })
+        .collect()
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+
+    parse_input(input).iter().map(|e| e.calories())
+        .max()
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let mut elves = parse_input(input)
+        .iter()
+        .map(|e| e.calories())
+        .collect::<Vec<u32>>();
+
+    elves.sort();
+
+    Some(elves.iter().rev().take(3).sum())
+
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 1);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file("examples", 1);
+        assert_eq!(part_one(&input), None);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 1);
+        assert_eq!(part_two(&input), None);
+    }
+}
